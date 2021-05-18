0dac0f61289fbfca925cf9da9e737268f3325fb8
Mr yuan <2205968704@qq.com>
2021-05-18 16:54:28 +0800

    helloThird123

--- src/com/bjpowernode/domain/Person.java
+++ src/com/bjpowernode/domain/Person.java
@@ -5,4 +5,42 @@ public class Person {
     private int id;
     private String emaill;
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", emaill='" + emaill + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmaill() {
+        return emaill;
+    }
+
+    public void setEmaill(String emaill) {
+        this.emaill = emaill;
+    }
+
+    public Person(String name, int id, String emaill) {
+        this.name = name;
+        this.id = id;
+        this.emaill = emaill;
+    }
 }
